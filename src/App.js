73bbc3109cbd2b524c6d0c5c5d7b01047a7e330c
youzyko <gokongjin@naver.com>
2023-02-06 11:25:00 +0900

    CRUD_complete

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 /* 사용자 정의 태그 (무조건 대문자로 시작) 
   사용자 정의 태그=컴포넌트 
 */
-
+//16분 21초
 function Header(props) {
   console.log("props", props, props.title);
   return (
@@ -62,44 +62,76 @@ function Article(props) {
   );
 } //article end
 
-function Create(props){
-  return <article>
-    <h2>Create</h2>
-    <form onSubmit={(event)=>{
-      event.preventDefault();
-      const title=event.target.title.value;
-      const body=event.target.body.value;
-      props.onCreate(title,body);
-    }}>
-     <p> <input type="text" name="title" placeholder="title"/></p>
-     <p><textarea name="body" placeholder="body"/></p>
-     <p> <input type="submit" value="Create"/></p>
-    </form>
-  </article>
-}
-function Update(props){
-  const[title,setTitle]=useState(props.title); //타이틀수정
-  const[body,setBody]=useState(props.body);
-  return(
+function Create(props) {
+  return (
     <article>
-    <h2>Update</h2>
-    <form onSubmit={(event)=>{
-      event.preventDefault();
-      const title=event.target.title.value;
-      const body=event.target.body.value;
-      props.onUpdate(title,body);
-    }}>
-     <p> <input type="text" name="title" placeholder="title" value={title} onChange={(event)=>{
-       
-        setTitle(event.target.value); //새로운 타이값으로 바꾸기
-     }}/></p>
-     <p><textarea name="body" placeholder="body" value={body} onChange={(event)=>{
-        setBody(event.target.value);
-     }}/></p>
-     <p> <input type="submit" value="Update"/></p>
-    </form>
-  </article>
-  )
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          {" "}
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body" />
+        </p>
+        <p>
+          {" "}
+          <input type="submit" value="Create" />
+        </p>
+      </form>
+    </article>
+  );
+}
+function Update(props) {
+  const [title, setTitle] = useState(props.title); //타이틀수정
+  const [body, setBody] = useState(props.body);
+  return (
+    <article>
+      <h2>Update</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onUpdate(title, body);
+        }}
+      >
+        <p>
+          {" "}
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            value={title}
+            onChange={(event) => {
+              setTitle(event.target.value); //새로운 타이값으로 바꾸기
+            }}
+          />
+        </p>
+        <p>
+          <textarea
+            name="body"
+            placeholder="body"
+            value={body}
+            onChange={(event) => {
+              setBody(event.target.value);
+            }}
+          />
+        </p>
+        <p>
+          {" "}
+          <input type="submit" value="Update" />
+        </p>
+      </form>
+    </article>
+  );
 }
 function App() {
   /* const _mode = useState("WELCOME"); //usestate는 배열 return /배열의0번째 요소는
@@ -109,15 +141,16 @@ function App() {
 
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  const [nextId,setNextId]=useState(4); //초기값은 4로 지정
+  const [nextId, setNextId] = useState(4); //초기값은 4로 지정
   const [topics, settopics] = useState([
     { id: 1, title: "html", body: "html is..." },
     { id: 2, title: "css", body: "css is..." },
     { id: 3, title: "js", body: "js is..." },
   ]);
   let content = null;
-  let contextControl=null;
-  if (mode === "WELCOME") {//초기
+  let contextControl = null;
+  if (mode === "WELCOME") {
+    //초기
     content = <Article title="Welcome" body="Hello,WEB"></Article>;
   } else if (mode === "READ") {
     let title,
@@ -129,21 +162,53 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
-    contextControl=  <li><a href={"/update/"+id} onClick={(event)=>{
-      event.preventDefault();
-      setMode('UPDATE');
-    }}>Update</a></li>
-  }else if(mode ==='CREATE'){
-    content=<Create onCreate={(_title,_body)=>{
-      const newTopic ={id:nextId, title:_title, body:_body};
-      const newTopics=[...topics];
-      newTopics.push(newTopic);
-      settopics(newTopics)
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1) 
-    }}></Create>
-  }else if(mode==='UPDATE'){
+    contextControl = (
+      <>
+        <li>
+          <a
+            href={"/update/" + id}
+            onClick={(event) => {
+              event.preventDefault();
+              setMode("UPDATE");
+            }}
+          >
+            Update
+          </a>
+        </li>
+
+        <li>
+          <input
+            type="button"
+            value="Delete"
+            onClick={() => {
+              const newTopics = [];
+              for (let i = 0; i < topics.length; i++) {
+                if (topics[i].id !== id) {
+                  newTopics.push(topics[i]);
+                }
+              }
+              settopics(newTopics);
+            }}
+          />
+        </li>
+      </>
+    );
+    //빈태그
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          settopics(newTopics);
+          setMode("READ");
+          setId(nextId);
+          setNextId(nextId + 1);
+        }}
+      ></Create>
+    );
+  } else if (mode === "UPDATE") {
     let title,
       body = null;
     for (let i = 0; i < topics.length; i++) {
@@ -152,11 +217,26 @@ function App() {
         body = topics[i].body;
       }
     }
-    content=<Update title={title} body={body} onUpdate={(title,body)=>{
-      console.log(title,body);
-    }}></Update>
-  }
- 
+    content = (
+      <Update
+        title={title}
+        body={body}
+        onUpdate={(title, body) => {
+          //console.log(title, body);
+          const newTopics = [...topics]; //복제
+          const updateTopic = { id: id, title: title, body: body }; //수정된 topic
+          for (let i = 0; i < newTopics.length; i++) {
+            if (newTopics[i].id == id) {
+              newTopics[i] = updateTopic;
+              break;
+            }
+          }
+          settopics(newTopics);
+        }}
+      ></Update>
+    );
+  } //update
+
   return (
     <div className="App">
       <Header
@@ -175,20 +255,19 @@ function App() {
       ></Nav>
       {content}
       <ul>
-      <li>
-      <a
-        href="/create"
-        onClick={(event) => {
-          event.preventDefault();
-          setMode('CREATE');
-        }}
-      >
-        Create
-      </a>
-      </li>
+        <li>
+          <a
+            href="/create"
+            onClick={(event) => {
+              event.preventDefault();
+              setMode("CREATE");
+            }}
+          >
+            Create
+          </a>
+        </li>
         {contextControl}
-    
-        </ul>
+      </ul>
     </div>
   );
 }
